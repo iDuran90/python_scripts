adf75a9ee4448ffb8f68bf823c2eba687b5b9cd8
Jorge Durán <jdp0990@gmail.com>
2019-03-19 19:12:56 -0500

    Tesis report script (#2)

---
+++ elements_categories.py
@@ -0,0 +1,335 @@
+gameElementsConvention = {
+    "Accessibility": [
+        "Access",
+        "Access instructional Material",
+        "Online game",
+        "Online games"
+    ],
+    "Adaptability": ["Adaptability"],
+    "Additional device to jump": ["Additional device to jump"],
+    "Adventure game": ["Adventure game", "Robot Worlds"],
+    "Allow mistakes": [
+        "Allow mistakes",
+        "Freedom to fail"
+    ],
+    "ARCS": [
+        "ARCS",
+        "ARCS (Attention, Relevance, Confidence, Satisfaction)"
+    ],
+    "Augmented Reality": [
+        "AR",
+        "Augmented reality",
+        "QR Codes",
+        "Real world objects"
+    ],
+    "AURASMA": ["AURASMA"],
+    "Blended Learning": [
+        "Flipped classroom",
+        "Mixed approach",
+        "Mixed environment (Real/Virtual)",
+        "Simulate real scenarios",
+        "Simulated scenario",
+        "Virtual goods",
+        "Virtual reality",
+        "Virtual worlds"
+    ],
+    "Board Game": [
+        "Based on a board game called Mahjong",
+        "Block elimination game",
+        "Block-clearing game",
+        "Blocks building",
+        "Board",
+        "Board game",
+        "Cards game",
+        "Clickers",
+        "Dungeons",
+        "PUZZLE BOARD METAPHOR",
+        "Puzzle",
+        "Puzzles",
+        "Slots",
+        "Words building"
+    ],
+    "Challenge": ["Challenge"],
+    "Chat": ["Chats records"],
+    "Children games": [
+        "Children games",
+        "Farm game",
+        "kids"
+    ],
+    "Clear goals": [
+        "Clear goals",
+        "clear goals"
+    ],
+    "Collaborative": [
+        "Collaboration",
+        "Collaborative",
+        "Cooperative",
+        "Cooperative learning",
+        "EGameFlow",
+        "Peer collaboration",
+        "Peer interaction",
+        "Team work",
+        "Teams"
+    ],
+    "Communication": [
+        "Communication",
+        "Info sharing",
+        "Multiplayer mode",
+        "Peer communication",
+        "communicative competence"
+    ],
+    "Competitive": [
+        "Battles",
+        "Combat",
+        "Competence",
+        "Competition",
+        "Competition with partners",
+        "Competitive",
+        "Competitive learning",
+        "question and answer competition"
+    ],
+    "Feedback": [
+        "Feedback",
+        "Feedback strategy",
+        "Gently correction",
+        "Peer feedback",
+        "Positive statements",
+        "Reinforcement",
+        "Repetition until correct",
+        "Self-evaluating",
+        "feedback",
+        "receptive-activation cycles"
+    ],
+    "Fill in the blank game": ["Fill in the blank"],
+    "Grammar Acquisition": ["Grammar"],
+    "Help Section": [
+        "Q&A",
+        "Q&A in English"
+    ],
+    "Hints": [
+        "Hints",
+        "Hints to follow"
+    ],
+    "Hypermedia": ["Hypermedia"],
+    "Interactive Whiteboard": ["IWB (Interactive whiteboard)"],
+    "Introductory level": ["Introductory level"],
+    "Kahoot!": [
+        "Kahoot",
+        "Kahoot!"
+    ],
+    "Leaderboard": [
+        "Honor roll",
+        "Leader board",
+        "Leader boards",
+        "Leaderboards",
+        "Rank",
+        "Ranking",
+        "Rating",
+        "Score",
+        "Scores",
+        "Scoring"
+    ],
+    "Learner centered": ["Learner centered"],
+    "Leveling": [
+        "Difficulty levels",
+        "Level",
+        "Levels"
+    ],
+    "Listening acquisition": ["Listening"],
+    "Location aware": [
+        "Context aware",
+        "GPS",
+        "GPS coordinates",
+        "Location questions",
+        "Map exploration",
+        "Map route",
+        "Maps",
+        "Mobile",
+        "Mobility",
+        "Questions for place",
+        "Situated Learning",
+        "Ubiquitous"
+    ],
+    "Location-based Game": [
+        "Capture flag",
+        "Chase-and-Catch",
+        "Follow-the-Path",
+        "Search-and-Find",
+        "Treasure hunt",
+        "Treasury hunt"
+    ],
+    "Match game": [
+        "Market game",
+        "Match game",
+        "Matching",
+        "Matching, pairs, bubbles, stickers, colouring"
+    ],
+    "MCQ Game": [
+        "MCQ",
+        "MCQ (multiple choice)",
+        "MCQs",
+        "Multiple choice"
+    ],
+    "Memory stimulation": [
+        "Memory",
+        "Memory stimulation"
+    ],
+    "Mingoville": ["Mingoville"],
+    "Missions game": [
+        "Mission",
+        "Missions"
+    ],
+    "Native language interface": [
+        "Narrated native language"
+    ],
+    "Non internet": [
+        "Non internet"
+    ],
+    "Notifications": [
+        "Notifications"
+    ],
+    "Observational Learning": [
+        "Observational Learning"
+    ],
+    "Open World": [
+        "Free learning path",
+        "Freedom"
+    ],
+    "Physical Iteration": [
+        "Body motion",
+        "Kinect",
+        "Physical interaction",
+        "Physical response"
+    ],
+    "Problem Solving Oriented": [
+        "Case-based reasoning",
+        "Problem-based",
+        "Task-based"
+    ],
+    "QuesTInSitu": [
+        "QuesTInSitu"
+    ],
+    "Quizlet": [
+        "Quizlet"
+    ],
+    "Reading acquisition": [
+        "Reading"
+    ],
+    "Replayability": [
+        "Free replay",
+        "Replay"
+    ],
+    "Rewards": [
+        "Achievements",
+        "Badges",
+        "Bonus",
+        "Content unlocking",
+        "Digital badges",
+        "Points",
+        "Progress bar",
+        "Rewards",
+        "Rewards (expected/unexpected)"
+    ],
+    "RPG Game": [
+        "Boss fights",
+        "Interactions with NPC",
+        "Learning roles",
+        "MMORPG",
+        "RPG",
+        "Virtual agents"
+    ],
+    "Social Networks Integration": [
+        "Discussions on Facebook",
+        "Facebook",
+        "Social",
+        "Social Networks",
+        "Social graphs",
+        "Social media",
+        "Social network",
+        "social interaction"
+    ],
+    "Sound Guide": [
+        "Audio-Lingual Method",
+        "Narrative",
+        "Sound",
+        "Sound guidance",
+        "Sound help",
+        "Sounds"
+    ],
+    "Speaking acquisition": [
+        "Speaking"
+    ],
+    "Speech Recognition": [
+        "Auditory and visual input",
+        "Pronunciation",
+        "Sound recording",
+        "Speech recognition",
+        "Speech recognizer"
+    ],
+    "Spelling Game": [
+        "Build words with a set of given letters",
+        "Spelling",
+        "Spelling game",
+        "Typing game"
+    ],
+    "Storytelling game": [
+        "Follow instructions game",
+        "Story-based",
+        "Storyline",
+        "Storytelling"
+    ],
+    "Time-free": [
+        "Free time",
+        "Off-school access"
+    ],
+    "Time-limited": [
+        "Time limited",
+        "Timer"
+    ],
+    "Usability": [
+        "Friendly UI",
+        "Student and teacher client",
+        "Teacher interface",
+        "Teachers control",
+        "Teachers interface",
+        "Usability",
+        "User centered design"
+    ],
+    "Use open content": [
+        "Use open content"
+    ],
+    "User Profiling": [
+        "Avatar",
+        "Content addapted to user information",
+        "Database to save students interactions data",
+        "Learning analytics",
+        "Personal data",
+        "Roles"
+    ],
+    "Video Capture": [
+        "Video capture"
+    ],
+    "Visual Guide": [
+        "Colorful images",
+        "Dialogues",
+        "Picture select",
+        "Pictures",
+        "Video",
+        "Videos contents",
+        "Visual",
+        "Visual help",
+        "Visual support"
+    ],
+    "Vocabulary Acquisition": [
+        "Vocabulary",
+        "Vocabulary acquisition",
+        "mnemonics for vocabulary",
+        "vocabulary, sentences and listening"
+    ],
+    "Web-oriented": [
+        "Web based"
+    ],
+    "WLAN": [
+        "WLAN"
+    ]
+}
--- reports.py
+++ reports.py
@@ -1,16 +1,40 @@
 import csv
 import yaml
+from elements_categories import gameElementsConvention
 
 with open('report_data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
+    gameElements = {}
+    gameElementsConventionKeys = gameElementsConvention.keys()
     for row in csv_reader:
-        if line_count == 0:
-            line_count += 1
-        elif line_count == 1:
+        if line_count != 0:
             yamlNotes = yaml.safe_load(row[3])
-            print(f'{yamlNotes}')
-            print(f'{yamlNotes["Inclusion"]}')
-            print(f'{yamlNotes["Exclusion"]}')
-            print(f'{yamlNotes["Research Question"]}')
-            line_count += 1
+            if type(yamlNotes) is dict:
+                if 'Research Question' in yamlNotes and yamlNotes['Research Question'] != None:
+                    elements = yamlNotes['Research Question'][0]['Elements']
+                    if type(elements) is list:
+                        for element in elements:
+                            if element in gameElementsConventionKeys:
+                                if element in gameElements:
+                                    gameElements[element] += 1
+                                else:
+                                    gameElements[element] = 1
+                            else:
+                                elementAdded = False
+                                for elementKey in gameElementsConventionKeys:
+                                    if element in gameElementsConvention[elementKey]:
+                                        if elementKey in gameElements:
+                                            elementAdded = True
+                                            gameElements[elementKey] += 1
+                                        else:
+                                            elementAdded = True
+                                            gameElements[elementKey] = 1
+                                if not elementAdded:
+                                    print('Elemento huerfano:' + str(element))
+        line_count += 1
+    print()
+
+    sorted_by_value = sorted(gameElements.items(), key=lambda kv: kv[1])
+    for key, value in reversed(sorted_by_value):
+        print(key + '\t' + str(value))
